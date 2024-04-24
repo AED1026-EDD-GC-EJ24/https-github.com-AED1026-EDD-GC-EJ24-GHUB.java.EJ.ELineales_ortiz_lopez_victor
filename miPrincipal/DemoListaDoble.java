0b3e60fcf2a6886bd8fd9eba222f838708dc4b84
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-04-24 16:03:59 +0000

    Lista doblemente ligada

--- miPrincipal/DemoListaDoble.java
+++ miPrincipal/DemoListaDoble.java
@@ -1,11 +1,20 @@
 package miPrincipal;
-
+import listaDoble.ListaDoblementeEnlazada;
 public class DemoListaDoble {
-    public static void menu()  {
+    public static void menu() throws PosicionIlegalException  {
         System.out.println("****************");
         System.out.println("  Lista Doble  ");
         System.out.println("****************");
         System.err.println();
+        ListaDoblementeEnlazada<Integer> listadoble = new ListaDoblementeEnlazada<Integer>();
+        listadoble.agregar(10);
+        listadoble.agregar(20);
+        listadoble.agregar(30);
+        System.out.println(listadoble.getValor(0));
+        System.out.println(listadoble.getValor(1));
+        System.out.println(listadoble.getValor(2));
+        
     }
+
     
 }
